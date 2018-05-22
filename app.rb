8909cb2ee74bca89e79107fcd6e8edce11b14112
ayesha ansari <ayesha.x.ansari@gmail.com>
2018-05-22 19:16:42 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/' do
-    "Welcome to your app!!!! Gladssssssssssssssssss"
+    "Welcome to your app!!!! Glad to see you"
   end
 
 end
